e99795e17383bacbb91cd4aaec137e602ac2a7ec
Ryan Ardray <rardray@gmail.com>
2019-01-17 21:08:33 -0600

    fix db?

--- db/migrate/20190116200259_create_drinks.rb
+++ db/migrate/20190116200259_create_drinks.rb
@@ -2,13 +2,9 @@ class CreateDrinks < ActiveRecord::Migration[5.2]
   def change
     create_table :drinks do |t|
       t.string :title
-      t.string :string
       t.string :description
-      t.string :string
       t.string :steps
-      t.string :string
       t.string :source
-      t.string :string
 
       t.timestamps
     end
